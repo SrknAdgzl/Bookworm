9f7746410d591ef79a060df97e9e4dfdd26b22a8
Serkan Adıgüzel <adgzl.srkn@gmail.com>
2023-08-29 15:15:02 +0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Serkan ADIGÜZEL on 29.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
