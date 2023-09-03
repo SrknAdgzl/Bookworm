f4ef856264046ac32f90a52601eac5240cda2cbe
SrknAdgzl <138037543+SrknAdgzl@users.noreply.github.com>
2023-09-03 19:26:11 +0300

    Science subject type is added.

--- Bookworm/AddBookView.swift
+++ Bookworm/AddBookView.swift
@@ -17,7 +17,7 @@ struct AddBookView: View {
     @State private var genre = ""
     @State private var review = ""
     
-    let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
+    let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller", "Science"]
     
     var body: some View {
         NavigationView {
